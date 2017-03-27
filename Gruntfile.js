2e901076c6ac36b7f8e00cd03501dffe8030b9f0
Tanisha Campbell <Tanisha Campbell>
2017-03-27 16:10:24 -0400

    got more specific and avoid looking into .eslintrc of any directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -413,7 +413,7 @@ module.exports = function (grunt) {
 		    'src/extensions/extra/HTMLHinter',
 		    'src/extensions/extra/MDNDocs',
 		    'src/extensions/extra/bramble-watch-index.html',
-		    '!src/extensions/extra/MDNDocs/tools/scrape-mdn/node_modules/**'	     
+		    '!src/extensions/extra/MDNDocs/tools/scrape-mdn/node_modules/**/.eslintrc'	     
 	    ],
             test:   '<%= meta.test %>',
             options: {
