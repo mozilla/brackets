bc221610f17fe1da4636e5b4216cfc12d961a966
Tanisha Campbell <Tanisha Campbell>
2017-03-28 11:43:22 -0400

    reversed order of extensions requests

--- Gruntfile.js
+++ Gruntfile.js
@@ -411,8 +411,8 @@ module.exports = function (grunt) {
 		    'src/extensions/default/bramble',
 		    'src/extensions/extra/SVGasXML',
 		    'src/extensions/extra/HTMLHinter',
-		    'src/extensions/extra/MDNDocs',
 		    '!src/extensions/extra/MDNDocs/**/node_modules/**',
+		    'src/extensions/extra/MDNDocs',
 		    'src/extensions/extra/bramble-watch-index.html'	     
 	    ],
             test:   '<%= meta.test %>',
