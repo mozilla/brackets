c9f38afdbb05337274eafafd1b9bd367b605b7c5
Tony Park <tonypark0403@gmail.com>
2017-04-01 23:33:39 -0400

    changed to the name of whitespace indicator simply to test

--- src/extensions/default/brackets-show-whitespace/main.js
+++ src/extensions/default/brackets-show-whitespace/main.js
@@ -44,8 +44,8 @@ define(function (require, exports, module) {
     
     // --- Settings ---
     
-    var commandID          = "denniskehrig.ShowWhitespace.toggle";
-    var preferencesID      = "denniskehrig.ShowWhitespace";
+    var commandID          = "allowWhiteSpace.toggle";
+    var preferencesID      = "allowWhiteSpace";
     var defaultPreferences = {
         enabled: false,
         colors: {
