723b7f75943177de1273535023cec00645c90313
harkirat <harkirat96@gmail.com>
2017-06-27 09:36:04 -0700

    Added initialization logic

--- src/editor/Collaboration.js
+++ src/editor/Collaboration.js
@@ -1,7 +1,7 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var SimpleWebRTC = require("simplewebrtc");
+    var SimpleWebRTC    = require("simplewebrtc");
 
     function Collaboration() {
         var webrtc = new SimpleWebRTC({
@@ -13,39 +13,88 @@ define(function (require, exports, module) {
             autoRequestMedia: false
         });
         this.webrtc = webrtc;
+        this.pending = []; // pending clients that need to be initialized.
+        this.changing = false;
     };
 
     Collaboration.prototype.init = function(codemirror) {
-        this.webrtc.joinRoom('thimble');
-        this.changing = false;
-        this.codemirror = codemirror;
         var self = this;
-        this.webrtc.connection.on('message', function (msg) {
-            if(msg.type !== "data" || self.changing) {
-                return;
-            }
-            var delta = msg.payload;
-            self.changing = true;
-            var cm = self.codemirror;
-            var start = cm.indexFromPos(delta.from);
-            // apply the delete operation first
-            if (delta.removed.length > 0) {
-                var delLength = 0;
-                for (var i = 0; i < delta.removed.length; i++) {
-                 delLength += delta.removed[i].length;
-                }
-                delLength += delta.removed.length - 1;
-                var from = cm.posFromIndex(start);
-                var to = cm.posFromIndex(start + delLength);
-                cm.replaceRange('', from, to);
-            }
-            // apply insert operation
-            var param = delta.text.join('\n');
-            var from = cm.posFromIndex(start);
-            var to = from;
-            cm.replaceRange(param, from, to);
-            self.changing = false;
+        this.webrtc.joinRoom('thimble', function() {
+            self.codemirror = codemirror;
+            self.webrtc.sendToAll("new client", {});
+            self.addListeners();
         });
+    };
+
+    Collaboration.prototype.addListeners = function() {
+        var self = this;
+        this.webrtc.on("createdPeer", function(peer) {
+            self.initialiseNewClient(peer);
+        });
+
+        this.webrtc.connection.on('message', function (msg) {
+            self.handleMessage(msg);
+        });
+    };
+
+    Collaboration.prototype.handleMessage = function(msg) {
+        if(msg.type === "new client") {
+            this.addToPending(msg.from);
+        }
+        if(this.changing) {
+            return;
+        }
+        if(msg.type === "data") {
+            this.handleCodemirrorChange(msg.payload);
+        }
+        if(msg.type === "initClient") {
+            this.initiseEditor(msg.payload);
+        }
+    };
+
+    Collaboration.prototype.addToPending = function(id) {
+        this.pending.push(id);
+    };
+
+    Collaboration.prototype.initiseEditor = function(value) {
+        this.changing = true;
+        this.codemirror.setValue(value);
+        this.changing = false;
+    };
+
+    Collaboration.prototype.initialiseNewClient = function(peer) {
+        this.changing = true;
+        for(var i = 0; i<this.pending.length; i++) {
+            if(this.pending[i] === peer.id) {
+                peer.send("initClient", this.codemirror.getValue());
+                this.pending.splice(i, 1);
+                break;
+            }
+        }
+        this.changing = false;
+    };
+
+    Collaboration.prototype.handleCodemirrorChange = function(delta) {
+        this.changing = true;
+        var cm = this.codemirror;
+        var start = cm.indexFromPos(delta.from);
+        // apply the delete operation first
+        if (delta.removed.length > 0) {
+            var delLength = 0;
+            for (var i = 0; i < delta.removed.length; i++) {
+             delLength += delta.removed[i].length;
+            }
+            delLength += delta.removed.length - 1;
+            var from = cm.posFromIndex(start);
+            var to = cm.posFromIndex(start + delLength);
+            cm.replaceRange('', from, to);
+        }
+        // apply insert operation
+        var param = delta.text.join('\n');
+        var from = cm.posFromIndex(start);
+        var to = from;
+        cm.replaceRange(param, from, to);
+        this.changing = false;
     };
 
     Collaboration.prototype.triggerCodemirrorChange = function(changeList) {
