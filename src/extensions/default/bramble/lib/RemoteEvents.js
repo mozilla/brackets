c1ec6f30625a85a87f89d0f652bd693bd8fe7518
Yogesh Gupta <yogesh201996@gmail.com>
2017-10-03 20:59:22 +0530

    Editor header displays empty message when no file is in editor
    Changed argument list of sendActiveEditorChange.
    When the last file in editor is deleted, sendActiveEditorChange function is called with empty strings. (Note : using null instead caused an error.)
    
    Using empty string avoids any need for localization.

--- src/extensions/default/bramble/lib/RemoteEvents.js
+++ src/extensions/default/bramble/lib/RemoteEvents.js
@@ -32,10 +32,7 @@ define(function (require, exports, module) {
         });
     }
 
-    function sendActiveEditorChangeEvent(file) {
-        var fullPath = file.fullPath;
-        var filename = Path.basename(fullPath);
-
+    function sendActiveEditorChangeEvent(fullPath, filename) {
         sendEvent({
             type: "bramble:activeEditorChange",
             fullPath: fullPath,
@@ -98,27 +95,27 @@ define(function (require, exports, module) {
         var lastKnownEditorFilePath;
         MainViewManager.on("currentFileChange", function(e, file) {
             if(!file) {
-                // This happens when the last file in working set is deleted.
                 lastKnownEditorFilePath = "";
-                let noFile = {
-                    fullPath: "No File",
-                    filename: "No File"
-                };
-                sendActiveEditorChangeEvent(noFile);               
+                sendActiveEditorChangeEvent("", "");
                 return;
             }
 
-            if(file.fullPath !== lastKnownEditorFilePath) {
-                lastKnownEditorFilePath = file.fullPath;
-                sendActiveEditorChangeEvent(file);
+            var fullPath = file.fullPath;
+            var filename = Path.basename(fullPath);
+
+            if(fullPath !== lastKnownEditorFilePath) {
+                lastKnownEditorFilePath = fullPath;
+                sendActiveEditorChangeEvent(fullPath, filename);
             }
         });
 
         // Listen for file rename
         BrambleEvents.on("fileRenamed", function(e, oldFilePath, newFilePath) {
             if (oldFilePath === lastKnownEditorFilePath) {
-                lastKnownEditorFilePath = newFilePath;
-                sendActiveEditorChangeEvent({ fullPath: newFilePath });
+                var fullPath = newFilePath;
+                var filename = Path.basename(fullPath);
+                lastKnownEditorFilePath = fullPath;
+                sendActiveEditorChangeEvent(fullPath, filename);
             }
         });
 
