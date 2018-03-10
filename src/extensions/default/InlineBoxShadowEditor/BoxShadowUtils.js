8b22e23dbf62c2c8644e35340077bdaa6c700037
Yogesh Gupta <yogesh201996@gmail.com>
2018-03-10 23:41:40 +0530

    Modified regex

--- src/extensions/default/InlineBoxShadowEditor/BoxShadowUtils.js
+++ src/extensions/default/InlineBoxShadowEditor/BoxShadowUtils.js
@@ -3,7 +3,7 @@ define(function(require, exports, module) {
 
     var DEFAULT_BOX_SHADOW_VALUE = " 0px 0px 0px 0px black";
     
-    var BOX_SHADOW_REGEX = /((-?\d+)(px|em|%)\s+){1,3}?((-?\d+)(px|em|%))(\s+([a-z]+))?(\s+inset)?/g;
+    var BOX_SHADOW_REGEX = /((-?\d+)(px|em|%)\s+){1,3}?((-?\d+)(px|em|%))(\s+([a-z]+))?(\s+inset)?|(inset\s+)?((-?\d+)(px|em|%)\s+){1,3}?((-?\d+)(px|em|%))(\s+([a-z]+))?|(([a-z]+)\s+)?((-?\d+)(px|em|%)\s+){1,3}?((-?\d+)(px|em|%))(\s+inset)?|(inset\s+)?(([a-z]+)\s+)?((-?\d+)(px|em|%)\s+){1,3}?((-?\d+)(px|em|%))/g;
     
     var LENGTH_REGEX = /(?:(-?\d+)(px|em|%))/g;
     
